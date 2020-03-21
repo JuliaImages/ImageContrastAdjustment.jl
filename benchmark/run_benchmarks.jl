5240e787ca7ef468077da8d3e693bbebc8bce224
Johnny Chen <johnnychen94@hotmail.com>
2020-03-22 03:18:37 +0800

    clean up old codes

--- benchmark/run_benchmarks.jl
+++ benchmark/run_benchmarks.jl
@@ -1,6 +1,10 @@
+# To run it locally, BenchmarkCI and JSON should be added to root project
 using BenchmarkCI
+using JSON
 on_CI = haskey(ENV, "GITHUB_ACTIONS")
 
+# github action workflow doesn't support action on labels
+# this function manually check if PR has label "run benchmarks"
 function should_do_benchmark(event_path = get(ENV, "GITHUB_EVENT_PATH", nothing))
     event_path === nothing && return false
     event = JSON.parsefile(event_path)
@@ -13,5 +17,5 @@ do_benchmark = on_CI ? should_do_benchmark() : true
 
 if do_benchmark
     BenchmarkCI.judge()
-    BenchmarkCI.displayjudgement()
+    on_CI ? BenchmarkCI.postjudge() : BenchmarkCI.displayjudgement()
 end
