98022cf5790b5d1b20a83f07d156b8c5af98576c
Johnny Chen <johnnychen94@hotmail.com>
2020-07-03 18:21:47 +0800

    use built-in label feature to trigger benchmark

--- benchmark/run_benchmarks.jl
+++ benchmark/run_benchmarks.jl
@@ -1,21 +1,6 @@
-# To run it locally, BenchmarkCI and JSON should be added to root project
+# To run it locally, BenchmarkCI should be added to root project
 using BenchmarkCI
-using JSON
 on_CI = haskey(ENV, "GITHUB_ACTIONS")
 
-# github action workflow doesn't support action on labels
-# this function manually check if PR has label "run benchmarks"
-function should_do_benchmark(event_path = get(ENV, "GITHUB_EVENT_PATH", nothing))
-    event_path === nothing && return false
-    event = JSON.parsefile(event_path)
-    haskey(event, "pull_request") || return false
-    labels = [x["name"] for x in event["pull_request"]["labels"]]
-    return "run benchmarks" in labels
-end
-
-do_benchmark = on_CI ? should_do_benchmark() : true
-
-if do_benchmark
-    BenchmarkCI.judge()
-    on_CI ? BenchmarkCI.postjudge() : BenchmarkCI.displayjudgement()
-end
+BenchmarkCI.judge()
+on_CI ? BenchmarkCI.postjudge() : BenchmarkCI.displayjudgement()
