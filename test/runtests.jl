7204c5077285948c5f8e73a2a63bf06564947147
Tim Holy <tim.holy@gmail.com>
2023-07-23 12:47:15 -0500

    Add Aqua tests; add [compat] for ImageBase (#61)
    The [compat] was missed in #60.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,13 @@
 using ImageContrastAdjustment
 using Test, ImageCore, ImageFiltering, TestImages, LinearAlgebra
+using Aqua
+
+if Base.VERSION >= v"1.6"
+    @testset "Aqua" begin
+        # TODO: fix the ambiguity test
+        Aqua.test_all(ImageContrastAdjustment; ambiguities=false)
+    end
+end
 
 @testset "ImageContrastAdjustment.jl" begin
     include("core.jl")
